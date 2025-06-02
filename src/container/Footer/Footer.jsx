18a4f2049e12955099ac0c806377c3e2a76e8be1
Dianawats <dianakiznaki@gmail.com>
2025-06-02 15:08:48 +0300

    update the newsletter and findus file

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
 import './Footer.css';
+import { FooterOverlay, Newsletter } from '../../components';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    {/* <h1>Test Footer</h1> */}
+    {/* <FooterOverlay /> */}
+    <Newsletter />
   </div>
 );
 
